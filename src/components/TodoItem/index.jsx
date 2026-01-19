510a40f12b9bd909f1cfd092281b452c383f4d80
Ahmed Hany <ahmedhanyelbaz@outlook.com>
2026-01-19 13:23:19 +0200

    feat(Main): add delete and mark as done abilites
    - add ability to delete a todo item and to mark it as completed/done.
    - make accompanying updates to TodoItem component.

---
+++ src/components/TodoItem/index.jsx
@@ -0,0 +1,14 @@
+function TodoItem(props) {
+    return (
+        <li>
+            <button onClick={() => props.onMarkAsDone(props.todo["id"])}>Complete</button>
+            <button onClick={() => props.onDeleteTodo(props.todo["id"])}>Delete</button>
+            <span>
+                {props.todo["done"] ? <s>{props.todo["todo"]}</s>
+                    : props.todo["todo"]}
+            </span>
+        </li>
+    );
+}
+
+export default TodoItem;
--- src/pages/main/index.jsx
+++ src/pages/main/index.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import TodoItem from '../../components/TodoItem/TodoItem';
+import TodoItem from '../../components/TodoItem';
 
 let todoId = 1;
 
@@ -7,21 +7,32 @@ function Main() {
     const [todoItems, setTodoItems] = useState([]);
 
     const addTodo = (todoItem) => {
-        const newTodoItems = [...todoItems, { id: todoId, todo: todoItem }];
+        const newTodoItems = [...todoItems, { id: todoId, todo: todoItem, done: false }];
         todoId++;
         setTodoItems(newTodoItems);
     };
 
+    const deleteTodo = (id) => {
+        const filteredTodoItems = todoItems.filter(todoItem => todoItem["id"] != id);
+        setTodoItems(filteredTodoItems);
+    };
+
+    const markAsDone = (id) => {
+        const completedTodoItem = todoItems.find(todoItem => todoItem["id"] == id);
+        completedTodoItem["done"] = true;
+        setTodoItems([...todoItems]);
+    };
+
     return (
         <>
-            <input type="text" />
-            <button type='submit' onClick={() => { addTodo("Todo Item"); }}>
+            <input type="text" placeholder="Enter a new task" id='taskInput' />
+            <button type='submit' onClick={ addTodo(`Todo Item ${todoId}`) }>
                 Add
             </button>
             <ul>
                 {todoItems.length == 0 ? <p>No todos yet.</p>
                     : todoItems.map(todoItem => {
-                        return <TodoItem key={todoItem["id"]} todo={todoItem["todo"]} />;
+                        return <TodoItem key={todoItem["id"]} todo={todoItem} onDeleteTodo={deleteTodo} onMarkAsDone={markAsDone} />;
                     }
                     )}
             </ul>
